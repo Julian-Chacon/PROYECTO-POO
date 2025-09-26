d06def79be817333dd7f738993198dcdcc9b9755
CENTIC <CENTIC@10.6.65.20>
2025-09-26 12:25:45 -0500

    HOLA

--- src/Booking/MainSystem.java
+++ src/Booking/MainSystem.java
@@ -6,15 +6,17 @@ public class MainSystem {
     
     public static void main(String[] args){
         
-        System.out.println("Prueba");
         
-        TransportService flight1 = new Flight();
+      
+        TransportService flight1 = new Flight(23, "bucaramanga", "bogota", 100.000, "avianca", "ejecutivo");
         TransportService bus1 = new Bus();
+        TransportService train1 = new Train();
         
         Passenger p1 = new Passenger();
         p1.addTransportService(bus1);
         p1.addTransportService(flight1);
-        
+        p1.addTransportService(train1);
+        System.out.println(flight1.toString());
     }
     
 }
