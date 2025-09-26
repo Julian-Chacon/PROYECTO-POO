3753a51b294944fdd698af67c751f34e2640328c
CENTIC <CENTIC@10.6.65.20>
2025-09-26 11:26:26 -0500

    HOLA

--- src/Booking/TransportService.java
+++ src/Booking/TransportService.java
@@ -1,6 +1,52 @@
 
 package Booking;
 
-public class TransportService {
+public abstract class TransportService {
+    private int id;
+    private String origin;
+    private String destination;
+    private double price;
     
+    public TransportService(){    
+    }
+    public TransportService(int id, String origin, String destination, double price){
+        this.setId(id);
+        this.setOrigin(origin);
+        this.setDestination(destination);
+        this.setPrice(price);
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setOrigin(String origin){
+        this.origin = origin;
+    }
+    public void setDestination(String destination){
+        this.destination = destination;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public int getId(){
+        return this.id;
+    }
+    public String getOrigin(){
+        return this.origin;
+    }
+    public String getDestination(){
+        return this.destination;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+    
+    @Override
+    public String toString(){
+        String str = "";
+        
+        return str;
+    }
+    
+    public abstract String getServiceInformation();
+    public abstract String getLuggageInformation();
 }
