3bb7e29c704b42d1dec0de20741185a088fa9a73
CENTIC <CENTIC@10.6.65.152>
2025-09-26 11:34:20 -0500

    hola

--- src/Booking/Train.java
+++ src/Booking/Train.java
@@ -2,6 +2,38 @@
 package Booking;
 
 
-public class Train {
+public class Train extends TransportService {
+    private String seatType;
+    private int wagonNumber;
     
+    public Train(){
+    }
+    public Train(int id, String origin, String destination, double price, String seatType, int wagonNumber){
+    super(id, origin, destination, price);
+    setSeatType(seatType);
+    setWagonNumber(wagonNumber);
+    }
+    public final void setSeatType(String seatType){
+        this.seatType = seatType;
+    }
+    public final void setWagonNumber(int wagonNumber){
+        this.wagonNumber = wagonNumber;
+    }
+    public String getSeatType(){
+        return this.seatType;
+    }
+    public int getWagonNumber(){
+        return this.wagonNumber;
+    }
+    public String getServiceInformation(){
+        return this.serviceInformation;
+    }
+    public String getLuggageInformation(){
+        return this.luggageInformation;
+    }
+    @Override
+    public String toString(){
+        String str = this.getSeatType() + this.getWagonNumber() + this.getServiceInformation() + this.getLuggageInformation() + super.toString();
+            return str;
+    }
 }
